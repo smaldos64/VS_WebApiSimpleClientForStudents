b82b919ce8df1f907f761be9046ea5b4cdb2e941
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-10-05 23:14:08 +0200

    Add project files.

---
+++ WebApiSimpleClientForStudents/IndexHelper.js
@@ -0,0 +1,11 @@
+var StudentOperation_ENUM;
+(function (StudentOperation_ENUM) {
+    StudentOperation_ENUM[StudentOperation_ENUM["Save_Student_Data"] = 0] = "Save_Student_Data";
+    StudentOperation_ENUM[StudentOperation_ENUM["Edit_Student_Data"] = 1] = "Edit_Student_Data";
+})(StudentOperation_ENUM || (StudentOperation_ENUM = {}));
+//const EditStudentNameClassName = "EditStudentName";
+//export class ProgramConstants {
+//    static readonly EditStudentNameClassName = "EditStudentName";
+//    static DeleteStudentNameClassName = "DeleteStudentName";
+//}
+//# sourceMappingURL=IndexHelper.js.map
---
+++ WebApiSimpleClientForStudents/IndexHelper.ts
@@ -0,0 +1,11 @@
+﻿enum StudentOperation_ENUM {
+    Save_Student_Data,
+    Edit_Student_Data
+}
+
+//const EditStudentNameClassName = "EditStudentName";
+
+//export class ProgramConstants {
+//    static readonly EditStudentNameClassName = "EditStudentName";
+//    static DeleteStudentNameClassName = "DeleteStudentName";
+//}
